d8651c8baf539c407547374ddd974c08feb56d51
Ilya Bylich <ibylich@gmail.com>
2025-07-01 16:56:23 +0200

    move tray handle to TLS

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ mod tray;
 fn main() -> Result<()> {
     MPClipboard::start()?;
     Exit::setup_handler()?;
-    let tray = Tray::new()?;
+    Tray::spawn()?;
 
     let mut timer = Timer::new(Duration::from_millis(100));
 
-    timer.add(1, move || {
+    timer.add(1, || {
         if Exit::received() {
             ControlFlow::Break(())
         } else {
@@ -23,32 +23,29 @@ fn main() -> Result<()> {
         }
     });
 
-    timer.add(1, {
-        let tray = tray.clone();
-        move || {
-            if let Some(event) = MPClipboard::recv() {
-                if let Some(connectivity) = event.connectivity {
-                    tray.set_connectivity(connectivity);
-                }
-
-                if let Some(text) = event.text {
-                    Clipboard::write(&text);
-                    tray.push_received(&text);
-                }
+    timer.add(1, || {
+        if let Some(event) = MPClipboard::recv() {
+            if let Some(connectivity) = event.connectivity {
+                Tray::set_connectivity(connectivity);
             }
-            ControlFlow::Continue(())
+
+            if let Some(text) = event.text {
+                Clipboard::write(&text);
+                Tray::push_received(&text);
+            }
         }
+        ControlFlow::Continue(())
     });
 
-    timer.add(10, move || {
+    timer.add(10, || {
         if let Some(text) = Clipboard::read() {
-            tray.push_local(&text);
+            Tray::push_local(&text);
             MPClipboard::send(text);
         }
         ControlFlow::Continue(())
     });
-    timer.start()?;
 
+    timer.start()?;
     log::info!("exiting...");
     if let Err(err) = MPClipboard::stop() {
         log::error!("{err:?}");
--- src/tray/mod.rs
+++ src/tray/mod.rs
@@ -2,32 +2,49 @@ mod buffer;
 mod event;
 mod state;
 
+use std::{cell::RefCell, rc::Rc};
+
 use anyhow::{Context as _, Result};
 use ksni::blocking::{Handle, TrayMethods};
 use state::TrayState;
 
-#[derive(Clone)]
-pub(crate) struct Tray {
-    handle: Handle<TrayState>,
+thread_local! {
+    static HANDLE: Rc<RefCell<Option<Handle<TrayState>>>> = Rc::new(RefCell::new(None));
+}
+fn update<F>(f: F)
+where
+    F: FnOnce(&mut TrayState),
+{
+    HANDLE.with(|handle| {
+        if let Some(handle) = handle.borrow().as_ref() {
+            handle.update(f);
+        } else {
+            log::error!("Tray handle is not set, did you call Tray::spawn() ?");
+            std::process::exit(1);
+        }
+    })
 }
 
+#[derive(Clone)]
+pub(crate) struct Tray;
+
 impl Tray {
-    pub(crate) fn new() -> Result<Self> {
+    pub(crate) fn spawn() -> Result<()> {
         let state = TrayState::new();
         let handle = state.spawn().context("failed to spawn Tray")?;
-        Ok(Self { handle })
+        HANDLE.with(|global| *global.borrow_mut() = Some(handle));
+        Ok(())
     }
 
-    pub(crate) fn push_local(&self, text: &str) {
-        self.handle.update(|tray| tray.push_local(text));
+    pub(crate) fn push_local(text: &str) {
+        update(|tray| tray.push_local(text));
     }
 
-    pub(crate) fn push_received(&self, text: &str) {
-        self.handle.update(|tray| tray.push_received(text));
+    pub(crate) fn push_received(text: &str) {
+        update(|tray| tray.push_received(text));
     }
 
-    pub(crate) fn set_connectivity(&self, connectivity: bool) {
-        self.handle
-            .update(|tray| tray.set_connectivity(connectivity));
+    pub(crate) fn set_connectivity(connectivity: bool) {
+        update(|tray| tray.set_connectivity(connectivity));
     }
 }
